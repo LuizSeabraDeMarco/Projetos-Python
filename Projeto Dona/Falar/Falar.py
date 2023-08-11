0efa0be94495919bffafdc0e37f17ac1d33168ce
LuizSeabraDeMarco <139667471+LuizSeabraDeMarco@users.noreply.github.com>
2023-08-11 16:14:46 -0300

    novas falas

--- Projeto Dona/Falar/Falar.py
+++ Projeto Dona/Falar/Falar.py
@@ -1,8 +1,25 @@
 import gtts
 from playsound import playsound
+import random
+import os
 
-with open('frase.txt', 'r') as arquivo:
-    for linha in arquivo:
-        frase = gtts.gTTS(linha, lang='pt-BR')
-        frase.save('frase.mp3')
-        playsound('frase.mp3')
+sorteio_fala = random.randint(1,2)
+sorteio_fala = 1
+print(sorteio_fala)
+
+
+if sorteio_fala < 0:
+    print('ERRO')
+elif sorteio_fala == 1:
+    file = open("frase.txt", 'r')  
+    with file as arquivo:
+        for linha in arquivo:
+            frase1 = gtts.gTTS(linha, lang='pt-BR')
+            frase1.save('frase.mp3')
+            playsound('frase.mp3')
+elif sorteio_fala == 2:
+    with open('frase2.txt', 'r') as arquivo2:
+        for linha2 in arquivo2:
+            frase2 = gtts.gTTS(linha2, lang='pt-BR')
+            frase2.save('frase2.mp3')
+            playsound('frase2.mp3')
