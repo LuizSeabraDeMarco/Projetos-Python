f0714ff9abcdaa6a4d58532c0161a71497bb2584
Luiz Filipe Seabra de marco <139667471+LuizSeabraDeMarco@users.noreply.github.com>
2023-09-19 13:22:09 -0400

    oi

---
+++ Classificacao/Classificar_imagem.py
@@ -0,0 +1,11 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+
